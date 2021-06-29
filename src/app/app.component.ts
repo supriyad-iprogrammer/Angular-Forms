973563e206c3f6e407c4c7065a30db01c9726444
supriyad-iprogrammer <supriyad@iprogrammer.co>
2021-06-29 15:44:38 +0530

    Complete Templayte Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Forms';
+  isSubmitted=false;
+  defaultvalue='pet';
+  answer='';
+  genders=['Male','Female'];
+  @ViewChild('form')
+  Registerform!: NgForm;
+  user={
+    username:'',
+    email:'',
+    secreteQuestion:'',
+    answer:'',
+    gender:''
+  }
+
+  onSubmit(){
+
+this.isSubmitted=true;
+this.user.username=this.Registerform.form.value.userData.username;
+this.user.email=this.Registerform.form.value.userData.email;
+this.user.secreteQuestion=this.Registerform.form.value.secret;
+this.user.answer=this.Registerform.form.value.answer;
+this.user.gender=this.Registerform.form.value.gender;
+
+  }
+  suggesstionUser(){
+
+    console.log("hiii")
+    const suggesstionUseename ='supriya';
+    this.Registerform.form.patchValue({
+userData:{
+  username:suggesstionUseename
+}
+    });
+
+  }
 }
